00546cee6e18c9c0f627fbf3314f0bb5bc77cbb6
Tunde Ajao <tunde_ajao@hotmail.com>
2022-09-11 15:11:13 +0100

    Adding Public files

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+app.use(express.static('public'));
 
 app.listen(3000, () => {
     console.log("App listening on port 3000");
 });
 
 app.get('/', (req, res) => {
-    res.json({
-        name: 'Greg Lim'
-    });
+    // res.json({
+    //     name: 'Greg Lim'
+    // });
+    res.sendFile(path.resolve(__dirname, 'index.html'));
 });
