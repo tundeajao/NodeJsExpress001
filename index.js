d5b54250c8c2b49b2cf8dd161cf6af15497c6a2f
Tunde Ajao <tunde_ajao@hotmail.com>
2022-09-11 16:29:44 +0100

    Adding the Routing Files

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ app.listen(3000, () => {
 });
 
 app.get('/', (req, res) => {
-    // res.json({
-    //     name: 'Greg Lim'
-    // });
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
 });
