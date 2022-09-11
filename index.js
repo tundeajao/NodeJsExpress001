0811ef36c99de0d4eb4d28779bee2132c7b561fb
Tunde Ajao <tunde_ajao@hotmail.com>
2022-09-11 14:45:01 +0100

    Addded Express and Get

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const app = express();
 app.listen(3000, () => {
     console.log("App listening on port 3000");
 });
+
+app.get('/', (req, res) => {
+    res.json({
+        name: 'Greg Lim'
+    });
+});
